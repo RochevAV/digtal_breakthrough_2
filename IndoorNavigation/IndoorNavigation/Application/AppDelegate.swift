7f487dd827ecef15ebd4a11b94e3e6975964e8c6
Aleksey Rochev <aleksey@rochev.studio>
2019-09-28 09:34:22 +0400

    Init project

---
+++ IndoorNavigation/IndoorNavigation/Application/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  IndoorNavigation
+//
+//  Created by Aleksey Rochev on 28.09.2019.
+//Copyright © 2019 Aleksey Rochev. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
